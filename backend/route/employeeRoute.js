78a2d593aa9a90b7f07b5ae9e0b0ba79d585be65
sumansauravmay <sumansauravmay12345@gmail.com>
2025-03-04 20:56:39 +0530

    delete

--- backend/route/employeeRoute.js
+++ backend/route/employeeRoute.js
@@ -23,5 +23,37 @@ employeeRouter.get("/employee/get", async (req, res) => {
     res.status(500).send({ msg: "Error" });
   }
 });
+
+
+employeeRouter.put("/employee/update/:id", async (req, res) => {
+    const ID = req.params.id;
+    try {
+      const updatedEmployee = await EmployeeModel.findByIdAndUpdate(ID, req.body, { new: true });
+      if (!updatedEmployee) {
+        return res.status(404).send({ msg: "Employee not found!" });
+      }
+      res.status(200).send({ msg: "Employee Updated!", employee: updatedEmployee });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ msg: "Update failed!" });
+    }
+  });
+  
+
+
+employeeRouter.delete("/employee/delete/:id", async (req, res) => {
+    const ID = req.params.id;
+    try {
+      await EmployeeModel.findByIdAndDelete({ _id: ID });
+      res.status(200).send({ msg: "Employee Deleted!" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ msg: "Can not be deleted" });
+    }
+  });
+  
+
+
+
 module.exports = { employeeRouter };
 
